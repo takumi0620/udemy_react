7b80e3a9c18fd4712927af15a051a94c068b531b
山本卓実 <yamamototakumi@koumotoukeinoMacBook-Pro.local>
2019-12-31 20:20:12 +0900

    30完了

---
+++ 30/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import './index.css';
+import reducer from './reducers';
+import EventsIndex from './components/events_index';
+import * as serviceWorker from './serviceWorker';
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <EventsIndex />
+  </Provider>,
+  document.getElementById('root')
+);
+serviceWorker.unregister();
